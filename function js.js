fe8e179466cc8e35b425e003456bff2964f131ae
gastonvr2023 <126197642+gastonvr2023@users.noreply.github.com>
2023-02-23 16:38:29 -0300

    Add files via upload
    cv henry rodriguez

---
+++ function js.js
@@ -0,0 +1,25 @@
+/**document.getElementById("boton").onclick = function ()
+{ console.log ("EVENTO");
+Document.getElementById ("demo") .innerHTML "EVENTO SOY HENRY "}
+
+*/
+
+document.getElementById('botoncolor').addEventListener('click', function () {
+document.body.style.backgroundColor = '#E0F021 ' 	} );
+
+
+document.getElementById('botoncolor2').addEventListener('click', function () {
+    document.body.style.backgroundColor = '#F02A21  ' 	} );
+    
+
+    document.getElementById('botoncolor3').addEventListener('click', function () {
+        document.body.style.backgroundColor = '#5DF021  ' 	} );
+    
+        document.getElementById('botoncolor4').addEventListener('click', function () {
+            document.body.style.backgroundColor = 'white  ' 	} );
+        
+
+            
+        document.getElementById('botoncolor5').addEventListener('click', function () {
+            document.body.style.backgroundColor = '#21EAF0   ' 	} );
+        
